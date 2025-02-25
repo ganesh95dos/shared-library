b752cbe5e121195d770bc2b9a5415673ac9db7b8
ganesh95dos <62937061+ganesh95dos@users.noreply.github.com>
2025-02-25 17:45:36 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,20 +1,3 @@
-def call(String DockerHubUser, String ProjectName, String ImageTag ){
- echo 'Hello, this is building the code'
- //Run the Docker build command
-    sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
- 
- // List images after building
-    sh 'docker images'
- // Remove untagged (dangling) images
-    sh 'docker image prune -f'
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
-
-                // Build the Docker image
-               // sh 'docker build -t my-django-note-app:latest .'
-
-                
-                
-                // Remove untagged (dangling) images
-               // sh 'docker image prune -f'
-
-               // echo 'Build code successfully'  // Echo for success message
