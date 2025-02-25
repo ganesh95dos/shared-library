fe9592f66ede98ed2e1eaa5bea58c2415c63c0be
ganesh95dos <62937061+ganesh95dos@users.noreply.github.com>
2025-02-25 16:00:43 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,7 @@
 def call(String DockerHubUser, String ProjectName, String ImageTag ){
  echo 'Hello, this is building the code'
  //Run the Docker build command
-    sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+    sh "docker build -t ${ProjectName}:${ImageTag} ."
  
  // List images after building
     sh 'docker images'
