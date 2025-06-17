ac459c63645f654ae7e540addbcf0d7755a955ea
ganesh95dos <62937061+ganesh95dos@users.noreply.github.com>
2025-06-17 18:12:35 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,9 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'Jenkins-app-note-django', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+def call(String project, String imageTag, String dockerHubUser) {
+  withCredentials([usernamePassword(credentialsId: 'Jenkins-app-note-django', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+    sh """
+      echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin
+      docker tag ${dockerHubUser}/${project}:${imageTag} \$DOCKER_USERNAME/${project}:${imageTag}
+      docker push \$DOCKER_USERNAME/${project}:${imageTag}
+    """
   }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
