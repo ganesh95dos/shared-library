52594cc331c2acf8aa7596e05d33dd42bb8b2321
ganesh95dos <62937061+ganesh95dos@users.noreply.github.com>
2025-11-18 12:41:01 +0530

    Fix docker tag command to use correct source image

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,7 +2,7 @@ def call(String project, String imageTag, String dockerHubUser) {
   withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
     sh """
       echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin
-      docker tag ${DOCKER_USERNAME}/${project}:${imageTag} \$DOCKER_USERNAME/${project}:${imageTag}
+      docker tag ${project}:${imageTag} \$DOCKER_USERNAME/${project}:${imageTag}
       docker push \$DOCKER_USERNAME/${project}:${imageTag}
     """
   }
