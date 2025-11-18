c4a0656252cf73d403dd99e86544b3d857fd6e15
ganesh95dos <62937061+ganesh95dos@users.noreply.github.com>
2025-11-18 12:19:44 +0530

    dockerhub-credentials update

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
 def call(String project, String imageTag, String dockerHubUser) {
-  withCredentials([usernamePassword(credentialsId: 'Jenkins-app-note-django', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+  withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
     sh """
       echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin
       docker tag ${dockerHubUser}/${project}:${imageTag} \$DOCKER_USERNAME/${project}:${imageTag}
