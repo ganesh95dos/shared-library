81ba60ad153cfecc5f9d5271bbe869b49e49eab0
ganesh95dos <62937061+ganesh95dos@users.noreply.github.com>
2025-11-19 12:12:59 +0530

    Refactor build.groovy to simplify docker commands

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,6 @@
 def call(String dockerHubUser, String ProjectName, String ImageTag ){
-{
+
  sh "docker build -t ${ProjectName}:${ImageTag} ."
- sh 'docker image prune -f'}
+ sh 'docker image prune -f'
  
 }
